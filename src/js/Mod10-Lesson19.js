5742e543d27b19f1c93cb5fb8abfd08d86785d4f
Oleg Adamov <olegadamov84@gmail.com>
2022-09-01 14:19:09 +0300

    Mod10

---
+++ src/js/Mod10-Lesson19.js
@@ -0,0 +1,135 @@
+    console.log('Repeta: ');
+
+{ console.log('Repeta: fetch-api');
+    /* 
+    * - HTTP-запросыв браузере
+    *  - Fetch API
+    *  - Вкладка Network
+    *  - HTTP-методы
+    *  - Заголовки
+    *  - MIME-типы
+    *  - Параметры запроса
+    * - Документация REST API
+    * - Обработка 404 c fetch
+    * - Аутентификация
+    * - Библиотеки-обёртки
+    * - https://pokeapi.co/
+    * - 
+    */
+
+
+
+    // import pokemonCardTpl from '';
+
+    // const refs = {
+    //     cardContainer: document.querySelector('.js-card-container'),
+    // }
+
+
+
+    fetch('https://pokeapi.co/api/v2/pokemon/2/')
+        .then(response => {
+        return response.json();
+    }).then(pokemon => {
+        console.log(pokemon);
+        //         const markup = pokemonCardTpl(pokemon);
+        //         console.log(markup)
+        //         refs.cardContainer.innerHTML = markup
+    })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+
+
+
+
+    {console.log('Example: ');
+
+
+
+}
+
+    {console.log('Example: ');
+
+
+
+}
+
+    {console.log('Example: ');
+
+
+
+}
+
+    {console.log('Example: ');
+
+
+
+}
+
+
+
+
+
+{console.log('Autocheck: ');
+
+
+
+
+}
+
+{console.log('Autocheck: ');
+
+
+
+
+}
+
+{console.log('Autocheck: ');
+
+
+    
+
+}
+
+
+
+
+
+{    console.log('Task-Konsp: REST API');
+
+    
+
+}
+
+{    console.log('Task-Konsp:');
+
+}
+
+{    console.log('Task-Konsp:');
+
+}
+
+{    console.log('Task-Konsp:');
+
+}
+
+{    console.log('Task-Konsp:');
+
+}
+
+{    console.log('Task-Konsp:');
+
+}
+
+
+{console.log(': ');
+
+    
+    
+    
+    
+    
+}
